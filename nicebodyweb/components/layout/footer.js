fafbe36b0ff6f5ce993081aaa56930a74085288e
yehch <10856004@ntub.edu.tw>
2024-03-11 16:30:28 +0800

    Home、robot(50%)

---
+++ nicebodyweb/components/layout/footer.js
@@ -0,0 +1,10 @@
+let renderFooter = function() {
+  let footerStr = `
+  <footer>
+    <img src="/images/logo.jpg" alt="">
+    <p>&copy;2024 臺北商業大學 資管系 無盡創新者</p>
+  </footer>
+  `;
+
+  document.getElementById("footer-container").innerHTML = footerStr;
+}
---
+++ nicebodyweb/components/layout/navbar.js
@@ -0,0 +1,36 @@
+let renderNav = function() {
+  let navStr = `
+    <nav>
+      <div class="area">
+        <div class="logo">
+          <img src="/images/logo.jpg" alt="">
+          <h1>Nice巴底</h1>
+        </div>
+        <div class="middle">
+          <div class="wrap">
+            <a href="/page/home/home.html">Home</a>
+            <a class="active" href="/page/robott/generateRecipes.html">Robot</a>
+            <a href="">Goal</a>
+            <a href="">Community</a>
+          </div>
+          <div class="icon">
+            <i class="fa-solid fa-bars" id="menuIcon"></i>
+            <i class="fa-solid fa-xmark" id="closeIcon"></i>
+          </div>
+        </div>
+        <div class="logSign">
+          <a href="">Log in</a>
+          <a href="">Sign up</a>
+        </div>
+      </div>
+      <div class="sideBar" id="sideBar">
+        <a href="">Home</a>
+        <a class="active" href="">Robot</a>
+        <a href="">Goal</a>
+        <a href="">Community</a>
+      </div>
+    </nav>
+  `;
+
+  document.getElementById("nav-container").innerHTML = navStr;
+}
