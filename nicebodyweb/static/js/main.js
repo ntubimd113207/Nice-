a3666c846f23d2bda4e9eb8db1482ccc3e950a84
yehch <10856004@ntub.edu.tw>
2024-04-24 20:23:41 +0800

    新增 - AI食譜上傳社群畫面

--- nicebodyweb/static/js/main.js
+++ nicebodyweb/static/js/main.js
@@ -35,45 +35,9 @@
 			}, 100);
 		});
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			mode: 'fade',
-			speed: 350,
-			noOpenerFade: true,
-			alignment: 'center'
-		});
 
 	// Scrolly.
 		$('.scrolly').scrolly();
-
-	// Nav.
-
-		// Button.
-			$(
-				'<div id="navButton">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
 
 	// Carousels.
 		$('.carousel').each(function() {
