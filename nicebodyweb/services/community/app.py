aea90904e3e0c33eee24872957ea2afd540e4850
yehch <10856004@ntub.edu.tw>
2024-05-18 20:42:06 +0800

    新增-社群主畫面(母畫面)、新增-單一貼文(子畫面)

--- nicebodyweb/services/community/app.py
+++ nicebodyweb/services/community/app.py
@@ -9,6 +9,18 @@ community_bp = Blueprint('community_bp', __name__)
 # 在目標服務藍圖加入路由
 #--------------------------
 
+#社群主頁(母畫面)
+@community_bp.route('/communityMain')
+def community_Main(): 
+    return render_template('/community/communityMain.html', data='王小明')
+
+
+#單一貼文(子畫面)
+@community_bp.route('/detailedPost')
+def detailed_post(): 
+    return render_template('/community/detailedPost.html', data='王小明')
+
+
 #AI食譜發布
 @community_bp.route('/airecipePost')
 def airecipe_post(): 
--- nicebodyweb/static/components/layout/navbar.js
+++ nicebodyweb/static/components/layout/navbar.js
@@ -1,44 +1,76 @@
 let renderNav = function() {
   let navStr = `
-    <nav>
-      <div class="area">
-        <div class="logo">
-          <img src="/static/images/logo.jpg" alt="nicebodylogo">
-          <h1>Nice巴底</h1>
+  <nav>
+    <div class="area">
+      <div class="logo">
+        <img src="/static/images/logo.jpg" alt="nicebodylogo">
+        <h1>Nice巴底</h1>
+      </div>
+      <div class="middle">
+        <div class="wrap">
+          <a href="/" class="nav-link">Home</a>
+          <a href="/robott/generateRecipes" class="nav-link">Robot</a>
+          <a href="/goal/goalMain" class="nav-link">Record</a>
+          <a href="/task/taskPage" class="nav-link">Task</a>
+          <a href="/community/communityMain" class="nav-link">Community</a>
         </div>
-        <div class="middle">
-          <div class="wrap">
-            <a href="/" class="nav-link">Home</a>
-            <a href="/robott/generateRecipes" class="nav-link">Robot</a>
-            <a href="/goal/goalMain" class="nav-link">Record</a>
-            <a href="/task/taskPage" class="nav-link">Task</a>
-            <a href="/community/airecipePost" class="nav-link">Community</a>
-          </div>
-          <div class="icon">
-            <i class="fa-solid fa-bars" id="menuIcon"></i>
-            <i class="fa-solid fa-xmark" id="closeIcon"></i>
-          </div>
-        </div>
-        <div class="logSign">
-          <a href="">Log in</a>
-          <a href="">Sign up</a>
-        </div>
-        <div class="alreadyLogin">
-          <a href="/profile/profilePage">
-            <img src="/static/images/user1.png" alt="">
-            <h3>${messageText}</h3>
-          </a>
-          <i class="fa-solid fa-right-from-bracket"></i>
+        <div class="icon">
+          <i class="fa-solid fa-bars" id="menuIcon"></i>
+          <i class="fa-solid fa-xmark" id="closeIcon"></i>
         </div>
       </div>
-    </nav>
-    <div class="sideBar" id="sideBar">
+      <div class="logSign">
+        <a href="">Log in</a>
+        <a href="">Sign up</a>
+      </div>
+      <div class="alreadyLogin">
+        <a href="/profile/profilePage">
+          <img src="/static/images/user1.png" alt="">
+          <h3>${messageText}</h3>
+        </a>
+        <i class="fa-solid fa-right-from-bracket"></i>
+      </div>
+    </div>
+  </nav>
+
+  <div class="comunitynav" id="comunitynav">
+    <div class="category">
+      <h6>All</h6>
+      <h6>Following</h6>
+      <h6>My</h6>
+    </div>
+    <select>
+      <option value="">all hashtag</option>
+      <option value="">小知識</option>
+    </select>
+    <div class="search">
+      <input type="search" placeholder="關鍵字查詢">
+      <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
+  </div>
+
+  <div class="sideBar" id="sideBar">
+    <div class="nav">
       <a href="/" class="nav-link">Home</a>
       <a href="/robott/generateRecipes" class="nav-link">Robot</a>
       <a href="/goal/goalMain" class="nav-link">Record</a>
       <a href="/task/taskPage" class="nav-link">Task</a>
-      <a href="/community/airecipePost" class="nav-link">Community</a>
+      <a href="/community/communityMain" class="nav-link">Community</a>
     </div>
+    <div class="sideBarcomunitynav">
+      <h6>All</h6>
+      <h6>Following</h6>
+      <h6>My</h6>
+      <select>
+        <option value="">all hashtag</option>
+        <option value="">小知識</option>
+      </select>
+      <div class="search">
+        <input type="search" placeholder="關鍵字查詢">
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  </div>
   `;
 
   document.getElementById("nav-container").innerHTML = navStr;
@@ -80,4 +112,11 @@ let renderNav = function() {
     closeIcon.style.display = 'none';
     menuIcon.style.display = 'block';
   });
+
+  const currentURL = window.location.pathname;
+  const communitynav = document.getElementById('communitynav');
+
+  if (currentURL === '/community/communityMain') {
+    communitynav.style.display = 'flex';
+  }
 }
