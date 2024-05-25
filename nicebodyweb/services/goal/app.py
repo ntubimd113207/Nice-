6864a9db2b99f844b3756fa42c695388a1d7f7c3
chiaoyu <10856042@ntub.edu.tw>
2024-05-25 17:27:03 +0800

    create - goalMain createCheckbox 前後端串接完成

--- nicebodyweb/services/goal/app.py
+++ nicebodyweb/services/goal/app.py
@@ -54,13 +54,39 @@ def goal_main():
         except Exception as e:
             return jsonify({'error': str(e)}), 500
 
+#打卡目標新增
+@goal_bp.route('/saveCheckbox', methods=['POST'])
+def save_goal():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
 
+            icon_id = data['iconId']
+            text = data['text']
+
+            connection = db.get_connection() 
+            cursor = connection.cursor()
+
+            cursor.execute('INSERT INTO body."checkCategory"("checkName", "Uid", "Iconid", create_time, update_time) VALUES(%s, 1, %s, now(), now())', (text, icon_id))
+            response = {'message': f'saveCheckbox inserted successfully.'}
+
+            connection.commit()
+            connection.close()
+
+            return jsonify(response)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return jsonify({'error': str(e)}), 500
 
 #打卡目標列表
 @goal_bp.route('/checkList')
 def check_list(): 
-    return render_template('/goal/checkList.html', data='王小明')
-
+    connection = db.get_connection() 
+    cursor = connection.cursor()     
+    cursor.execute('SELECT "ChCategoryid", "checkName", "Iconid" FROM body."checkCategory" where "Uid" = 1 order by create_time;')
+    data = cursor.fetchall()
+    connection.close()   
+    return render_template('/goal/checkList.html', data=data)
 
 #體重紀錄列表
 @goal_bp.route('/weightList')
