f1d3aac96c98469731ce67bb1ba388de92c52ed1
chiaoyu <10856042@ntub.edu.tw>
2024-06-09 10:23:17 +0800

    1. goalMain 新增今日體重 2. recipeWorld + shareResults 按讚

--- nicebodyweb/services/robott/app.py
+++ nicebodyweb/services/robott/app.py
@@ -43,7 +43,7 @@ def robott_everyList():
     connection = db.get_connection() 
 
     cursor = connection.cursor()     
-    cursor.execute('select * from body."v_recipeWorld" order by create_time desc;')
+    cursor.execute('select title, a.create_time, summary, "cookImage", likecount, messagecount, "userImage", diet, a."Cookid", "cookTime", "prepareMoney", a."Uid", COALESCE(b."Uid", 0) as cookbookLike from body."v_recipeWorld" as a left join (SELECT * FROM body."cookbookLike" where "Uid"=%s) as b on a."Cookid" = b."Cookid" and a."Uid" = b."Uid";', (1,))
 
     data = cursor.fetchall()
 
@@ -91,10 +91,8 @@ def robott_share():
     cursor = connection.cursor()     
 
     recipe_id = request.args.get('recipe_id')
-    likecount = request.args.get('likecount')
-    messagecount = request.args.get('messagecount')
 
-    cursor.execute('SELECT title, TO_CHAR(create_time, \'MM.DD.YYYY\'), summary, "prepare", "cookTime", "cookStep", nutrition, "cookImage", diet, "prepareMoney" FROM body."cookbook" where "Cookid" =%s', (recipe_id,))
+    cursor.execute('select title, a.create_time, summary, "prepare", "cookTime", "cookStep", nutrition, "cookImage", diet, "prepareMoney", a."Cookid", a."Uid", COALESCE(b."Uid", 0) as cookbookLike, likecount, messagecount from (SELECT title, create_time, summary, "prepare", "cookTime", "cookStep", nutrition, "cookImage", diet, "prepareMoney", "Cookid", "Uid", likecount, messagecount FROM body."v_recipeWorld" where "Cookid" = %s) as a left join (SELECT * FROM body."cookbookLike" where "Uid" = %s) as b on a."Cookid" = b."Cookid" and a."Uid" = b."Uid";', (recipe_id,1))
 
     data = cursor.fetchone()
 
@@ -104,4 +102,44 @@ def robott_share():
 
     connection.close()
 
-    return render_template('/robott/shareResults.html', data=data, data2=data2, Recipes_image_path=Recipes_image_path, user_image_path=user_image_path, likecount=likecount, messagecount=messagecount)
+    return render_template('/robott/shareResults.html', data=data, data2=data2, Recipes_image_path=Recipes_image_path, user_image_path=user_image_path)
+
+#發佈食譜 - 按讚
+@robott_bp.route('/likeAdd', methods=['POST'])
+def robott_likeAdd():
+    if request.method == 'POST':
+        try:
+            recipe_id = request.form.get('recipe_id')
+
+            connection = db.get_connection() 
+            cursor = connection.cursor()
+
+            cursor.execute('INSERT INTO body."cookbookLike" ("Cookid", "Uid", create_time) VALUES (%s, %s, now());', (recipe_id, 1))
+            response = {'message': f'likeAdd successfully.'}
+
+            connection.commit()
+            connection.close()
+
+            return jsonify(response)
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+        
+#發佈食譜 - 取消讚
+@robott_bp.route('/likeSub', methods=['POST'])
+def robott_likeSub():
+    if request.method == 'POST':
+        try:
+            recipe_id = request.form.get('recipe_id')
+
+            connection = db.get_connection() 
+            cursor = connection.cursor()
+
+            cursor.execute('DELETE FROM body."cookbookLike" WHERE "Cookid"=%s and "Uid"=%s;', (recipe_id, 1))
+            response = {'message': f'likeSub successfully.'}
+
+            connection.commit()
+            connection.close()
+
+            return jsonify(response)
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
