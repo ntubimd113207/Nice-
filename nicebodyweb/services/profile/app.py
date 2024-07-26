41edc188312bdcd9b9c9ed430da6a4d5a6b17bba
chiaoyu <10856042@ntub.edu.tw>
2024-07-27 02:04:13 +0800

    goal - 新增體重紀錄, profile - 查詢完成

--- nicebodyweb/services/profile/app.py
+++ nicebodyweb/services/profile/app.py
@@ -1,5 +1,6 @@
 # 匯入Blueprint模組
-from flask import render_template, Blueprint, session
+from flask import render_template, Blueprint, session, jsonify
+from utils import db
 
 # 產生目標服務藍圖
 profile_bp = Blueprint('profile_bp', __name__)
@@ -18,7 +19,16 @@ def profile_page():
         name='0'
         userImage='0'
 
-    return render_template('/profile/profilePage.html', name=name, userImage=userImage)
+    connection = db.get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT a.level_id, point, upgrade_point, a.gender, a.birthday FROM body.user_profile as a left join body.level_list as b on a.level_id = b.level_id where "Uid" = %s;', (session['uid'],))
+
+    profile_data = cursor.fetchone()
+
+    connection.close()
+
+    return render_template('/profile/profilePage.html', name=name, userImage=userImage, profile_data=profile_data)
 
 
 #關注列表頁面
