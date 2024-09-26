c52b0771451da57981c190dcd96ac1f641861d70
yehch <10856004@ntub.edu.tw>
2024-09-27 02:58:22 +0800

    Remove private files for public release

--- 瑄html稿/不要動/login.js
+++
@@ -1,9 +0,0 @@
-// Callback function for successful sign-in
-function onSignIn(googleUser) {
-  // Handle sign-in logic here, e.g., send user data to server
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do something with the user's ID
-  console.log('Name: ' + profile.getName()); // Do something with the user's name
-  console.log('Image URL: ' + profile.getImageUrl()); // Do something with the user's image URL
-  console.log('Email: ' + profile.getEmail()); // Do something with the user's email
-}
